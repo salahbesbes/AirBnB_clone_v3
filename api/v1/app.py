773f0b67a25c57b5e7a53a7e0e4eedf002ea30be
salahbesbes <salah_besbes@ymail.com>
2021-05-09 03:21:49 +0200

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
