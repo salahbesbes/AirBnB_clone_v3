4e332b78b8171c215c3661cc126558ecd10b68a0
salahbesbes <salah_besbes@ymail.com>
2021-05-09 04:58:02 +0200

    task1

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ modules """
 from os import getenv
 from flask import Flask
-from flask import jsonify
+from flask.json import jsonify
 from werkzeug.exceptions import HTTPException
 from models import storage
 from api.v1.views import app_views
@@ -10,7 +10,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -19,14 +19,22 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.errorhandler(404)
-def page_404(error):
-    """ Return a custom 404 error """
-    err_dict = {"error": "Not found"}
-    return jsonify(err_dict), 404
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # TODO: explore the e Exception methods
+    # this methode get called when ever abort() is called
+    # return jsonify({
+    #     "code": e.code,
+    #     "name": e.name,
+    #     "description": e.description,
+    # })
+    # if e.code == 404:
+    #     return jsonify({"error": "Not Found"})
+    return jsonify({"error": e.description})
 
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default='5000')
-    app.run(port=port, host=host, threaded=True)
+    app.run(port=port, host=host, threaded=True, debug=True)
