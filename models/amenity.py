d920b636e42954725f011a790d2857cb5048d0eb
salahbesbes <salah_besbes@ymail.com>
2021-05-09 18:03:36 +0200

    fix amenities

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,7 @@
 """ holds class Amenity"""
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -15,6 +12,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
     else:
         name = ""
+        place_id = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
