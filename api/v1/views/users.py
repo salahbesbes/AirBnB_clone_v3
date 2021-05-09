dc067211352de206ec64b46984e85300e38fe12a
eya-98 <eya.nani@sesame.com.tn>
2021-05-09 17:21:35 +0000

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -15,4 +15,45 @@ def users():
     result = []
     for obj in storage.all(User).values():
         result.append(obj.to_dict())
-    return jsonify(result)
+    return (jsonify(result))
+
+@get.route('/users/<string:user_id>', methods=['GET'], strict_slashes=False)
+def get():
+    """ Retrieves the list of all User objects """
+    result = storage.get(User, user_id)
+    if result is None:
+        abort (404)
+    return (jsonify(result.to_dict()))
+
+@delete.route('/users/<string:user_id>', method=['GET', 'DELETE'], strict_slashes=False)
+def delete():
+    """Delete a User object"""
+    result = storage.get(User, user_id)
+    if result is None:
+        abort (404)
+    storage.delete()
+    storage.save()
+    return (jsonify({}), 200)
+
+@creare.route('/users/<string:user_id>', method=['POST'])
+def post():
+    """transform the HTTP body request in json format 
+    to a dictionary containing the items email and password
+    """
+    if request.get_json() is None:
+        return (jsonify({'error' : 'Not a JSON', 400)
+    i = 0
+    j = 0
+    for key in request.get_json().keys():
+        if key == 'email':
+            mail += 1
+        if key == 'password'
+            pwd += 1
+    if mail < 1:
+        return (jsonify({'error': 'Missing email'}), 400)
+    if pwd < 1:
+        return (jsonify({'error': 'Missing password'}), 400)
+    new = User(email=request.get_json(['email']), 
+               password=request.get_json(['password'])
+    new.save()
+    return (jsonify(new.to_dict()), 201)
